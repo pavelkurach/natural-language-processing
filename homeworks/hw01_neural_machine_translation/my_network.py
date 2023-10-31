d39080856d50cfdbca7c09ba6eb1c85ff664f2c0
Pavel Kurach <pavel.kurach1@gmail.com>
2023-10-31 22:57:10 +0100

    Add lr scheduler

--- homeworks/hw01_neural_machine_translation/my_network.py
+++ homeworks/hw01_neural_machine_translation/my_network.py
@@ -10,6 +10,8 @@ import random
 import math
 import time
 
+import numpy as np
+
 
 class EncoderBlock(nn.Module):
     def __init__(self, embed_dim, num_heads, dim_feedforward, dropout=0.0):
@@ -243,3 +245,21 @@ class PositionalEncoding(nn.Module):
     def forward(self, x):
         x = x + self.pe[: x.size(0)]
         return self.dropout(x)
+    
+#from https://uvadlc-notebooks.readthedocs.io/en/latest/tutorial_notebooks/tutorial6/Transformers_and_MHAttention.html
+class CosineWarmupScheduler(optim.lr_scheduler._LRScheduler):
+
+    def __init__(self, optimizer, warmup, max_iters):
+        self.warmup = warmup
+        self.max_num_iters = max_iters
+        super().__init__(optimizer)
+
+    def get_lr(self):
+        lr_factor = self.get_lr_factor(epoch=self.last_epoch)
+        return [base_lr * lr_factor for base_lr in self.base_lrs]
+
+    def get_lr_factor(self, epoch):
+        lr_factor = 0.5 * (1 + np.cos(np.pi * epoch / self.max_num_iters))
+        if epoch <= self.warmup:
+            lr_factor *= epoch * 1.0 / self.warmup
+        return lr_factor
